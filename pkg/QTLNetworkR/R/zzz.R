9773689a20bdd1e680a497a1593eeff1f6b1bc26
wjzheng (Mr ) <wjzheng09@gmail.com>
2010-03-04 13:08:11 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rqtlnetwork@30 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/QTLNetworkR/R/zzz.R
+++
@@ -1,5 +0,0 @@
-.onLoad <- function(...) {
-  ## use .onLoad rather than .First.lib when there is namespace
-  cat("\nUse 'QTLNetworkR()' to restart the programe.\n",fill=TRUE)
-  if (interactive()) QTLNetworkR()
-}
